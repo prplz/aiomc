e83285b7fb262f1196114e4b5893c7299ec0894f
Michael Himing <mhiming@gmail.com>
2018-09-05 14:29:09 +1000

    Don't resolve the A record

--- aiomc/resolver.py
+++ aiomc/resolver.py
@@ -1,5 +1,3 @@
-import random
-from ipaddress import AddressValueError, IPv4Address
 from typing import Tuple
 
 from dns import resolver
@@ -17,10 +15,11 @@ def resolve_minecraft_server(address) -> Tuple[str, int]:
         host = parts[0]
         port = int(parts[1])
 
-    # if no port is supplied use 25565 or an SRV record if it exists
+    # if no port is supplied use 25565
     if port is None:
         port = 25565
 
+    if port == 25565:
         # attempt to find an SRV record for this host
         try:
             answers = resolver.query('_minecraft._tcp.' + host, 'SRV')
@@ -28,16 +27,8 @@ def resolve_minecraft_server(address) -> Tuple[str, int]:
                 answer = answers[0]
                 host = str(answer.target).rstrip(".")
                 port = int(answer.port)
+                return host, port
         except DNSException:
             pass
 
-    # return address pair if host is a valid IPv4 address
-    try:
-        IPv4Address(host)
-        return host, port
-    except AddressValueError:
-        pass
-
-    # pick a random A record for this host
-    answer = random.choice(resolver.query(host, 'A'))
-    return answer.address, port
+    return host, port
