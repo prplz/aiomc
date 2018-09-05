46d7242175d744d72dcb9c0fbba1cd0dbb319866
Michael Himing <mhiming@gmail.com>
2018-09-05 11:37:00 +1000

    Don't resolve A record in except block + comments

--- aiomc/resolver.py
+++ aiomc/resolver.py
@@ -9,14 +9,19 @@ from dns.exception import DNSException
 def resolve_minecraft_server(address) -> Tuple[str, int]:
     host = address
     port = None
+
     if ':' in address:
         parts = address.split(':')
         if len(parts) > 2:
             raise ValueError("Invalid address '%s'" % address)
         host = parts[0]
         port = int(parts[1])
+
+    # if no port is supplied use 25565 or an SRV record if it exists
     if port is None:
         port = 25565
+
+        # attempt to find an SRV record for this host
         try:
             answers = resolver.query('_minecraft._tcp.' + host, 'SRV')
             if answers:
@@ -25,9 +30,14 @@ def resolve_minecraft_server(address) -> Tuple[str, int]:
                 port = int(answer.port)
         except DNSException:
             pass
+
+    # return address pair if host is a valid IPv4 address
     try:
         IPv4Address(host)
+        return host, port
     except AddressValueError:
-        answer = random.choice(resolver.query(host, 'A'))
-        host = answer.address
-    return host, port
+        pass
+
+    # pick a random A record for this host
+    answer = random.choice(resolver.query(host, 'A'))
+    return answer.address, port
