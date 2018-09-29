587a0e9821f4ccbbdb185be16291ea03dbc76aa3
Michael Himing <mhiming@gmail.com>
2018-09-29 12:21:03 +1000

    Ensure transport gets closed

--- aiomc/ping.py
+++ aiomc/ping.py
@@ -15,28 +15,30 @@ async def minecraft_ping(host, port, loop: asyncio.AbstractEventLoop = None) -> 
         loop = asyncio.get_event_loop()
 
     conn = MinecraftProtocol(loop)
-    await loop.create_connection(lambda: conn, host, port)
+    try:
+        await loop.create_connection(lambda: conn, host, port)
 
-    conn.write_packet(packets.handshake(47, host, port))
-    conn.write_packet(packets.request_status())
-    await conn.drain()
+        conn.write_packet(packets.handshake(47, host, port))
+        conn.write_packet(packets.request_status())
+        await conn.drain()
 
-    packet = MinecraftIO(await conn.read_packet())
-    assert packet.read_varint() == 0
-    status = packet.read_string()
+        packet = MinecraftIO(await conn.read_packet())
+        assert packet.read_varint() == 0
+        status = packet.read_string()
 
-    ping_id = int(time.time() * 1000)
-    conn.write_packet(packets.ping(ping_id))
-    await conn.drain()
+        ping_id = int(time.time() * 1000)
+        conn.write_packet(packets.ping(ping_id))
+        await conn.drain()
 
-    ping_send_time = loop.time()
+        ping_send_time = loop.time()
 
-    packet = MinecraftIO(await conn.read_packet())
-    latency = int((loop.time() - ping_send_time) * 1000)
-    assert packet.read_varint() == 1
+        packet = MinecraftIO(await conn.read_packet())
+        latency = int((loop.time() - ping_send_time) * 1000)
+        assert packet.read_varint() == 1
 
-    conn.writer.close()
+        json_ = json.loads(status)
 
-    json_ = json.loads(status)
-
-    return PingResult(json_, latency)
+        return PingResult(json_, latency)
+    finally:
+        if conn.writer is not None:
+            conn.writer.close()
